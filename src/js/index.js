337f2ec6d4a9b841165d5ab792e25b7d1790d632
koallen <weifengzi2009@gmail.com>
2016-06-21 17:34:57 +0200

    add some simple angular code

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,6 @@
 var planner = angular.module('planner', []);
+
+planner.controller('TimetableCtrl', function($scope) {
+  $scope.slots = ["CZ2003", "CZ3005", "CZ4011", "CZ4003", "CZ4016", "ET0001"];
+  $scope.days = ["MON", "TUE", "WED", "THU", "FRI", "SAT"];
+});
